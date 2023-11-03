50b3ad9f36325ff9daaa27eef7fe1fa439eeb5d5
Squareface <squareface.officiel@gmail.com>
2023-11-03 14:04:06 +0100

    Désactivation de l'option d'ajouter et supprimer des élément dans le crud

---
+++ backend-symfony/src/Controller/Admin/InfosEntrepriseEntityCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\InfosEntrepriseEntity;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class InfosEntrepriseEntityCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return InfosEntrepriseEntity::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions->disable(Action::NEW);
+        $actions->disable(Action::DELETE);
+
+        return $actions;
+    }
+}
